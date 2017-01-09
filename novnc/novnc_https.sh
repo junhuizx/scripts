b35c4743703f4786fb2e13dfe95e112ba0939710
zjh <junhui.zhang@newtouch.cn>
2017-01-09 09:14:35 +0800

    use nova cmd to get hosts

--- novnc/novnc_https.sh
+++ novnc/novnc_https.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 set -x
 
-#mkdir ssl
-#openssl genrsa -out ssl/server.key 2048
-#openssl req -new -key ssl/server.key -out ssl/server.csr  -subj /C=CN/ST=newtouch/L=newtouch/O=ntx/CN=newtouch.com
-#openssl req -x509 -days 3650 -key ssl/server.key -in ssl/server.csr -out ssl/server.crt
+if [ ! -d ssl ]; then
+	mkdir ssl
+	openssl genrsa -out ssl/server.key 2048
+	openssl req -new -key ssl/server.key -out ssl/server.csr  -subj /C=CN/ST=newtouch/L=newtouch/O=ntx/CN=newtouch.com
+	openssl req -x509 -days 3650 -key ssl/server.key -in ssl/server.csr -out ssl/server.crt
+fi
 
 sshcmd="ssh -o StrictHostKeyChecking=no"
 scpcmd="scp -o StrictHostKeyChecking=no"
 source keystonerc_admin
-for host in `openstack compute service list --service  nova-consoleauth | awk -F\| '{if($2~"[0-9]+")print $4}'`; do
+for host in `nova service-list --binary nova-consoleauth  | awk -F\| '{if($2~"[0-9]+")print $4}'`; do
 	sshcmd_host="$sshcmd $host"
 	$scpcmd -r ssl $host:/etc/nova/
 	$sshcmd_host 'sed -i  '\''/#ssl_only *= *false/{n;/^ *ssl_only/'\!'{i \ssl_only=true
@@ -28,7 +30,7 @@ for host in `openstack compute service list --service  nova-consoleauth | awk -F
 	$sshcmd_host 'systemctl restart openstack-nova-novncproxy'
 done
 
-for host in `openstack hypervisor list | awk -F\| '{if($2~"[0-9]+")print $3}'`; do
+for host in `nova service-list --binary nova-compute  | awk -F\| '{if($2~"[0-9]+")print $4}'`; do
 openstack hypervisor show $host -c host_ip | grep host_ip | awk '{print $4}'
 $sshcmd $host "sed -i  's/^novncproxy_base_url *= *http:/novncproxy_base_url=https:/g' /etc/nova/nova.conf; systemctl restart openstack-nova-compute"
 done
