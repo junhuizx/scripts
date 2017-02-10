ab499d1e5ea15cef197bffa3fecc7fb1f1ed34d4
zjh <junhui.zhang@newtouch.cn>
2017-02-10 13:45:44 +0800

    install pika

--- rabbitmq/install.sh
+++ rabbitmq/install.sh
@@ -3,6 +3,7 @@
 mkdir rabbitmq_consumer
 cd rabbitmq_consumer
 yum install -y wget
+pip install pika
 wget https://github.com/junhuizx/scripts/archive/master.zip
 unzip master.zip
 cd scripts-master/rabbitmq/
