7b6a525797eb895bce773d832f5872b61687a674
zjh <zjh@zjh>
2017-02-17 15:03:24 +0800

    fix bugs in install_agent.sh

--- vmagent/install_agent.sh
+++ vmagent/install_agent.sh
@@ -11,16 +11,17 @@ curl https://git.newtouch.com/user6358/instance_monitor_agent/repository/archive
 for host in `nova service-list --binary nova-compute  | awk -F\| '{if($2~"[0-9]+")print $4}'`; do
     echo $host
     openstack hypervisor show $host -c host_ip | grep host_ip | awk '{print $4}'
-	ssh $host "rm -rf instance_monitor_agent.git agent.zip"
-	scp agent.zip $host:~/
-	scp /etc/yum.repos.d/CentOS* $host:/etc/yum.repos.d/
-	ssh $host "if ! grep nameserver /etc/resolv.conf ;then echo 'nameserver 114.114.114.114' >> /etc/resolv.conf ; systemctl restart network; fi"
-	ssh $host "yum clean all"
-	ssh $host "yum install -y epel-release"
-	ssh $host "yum install -y python-pip"
-	ssh $host "pip install pymongo"
+    ssh $host "rm -rf instance_monitor_agent.git agent.zip"
+    scp agent.zip $host:~/
+    scp /etc/yum.repos.d/CentOS* $host:/etc/yum.repos.d/
+    ssh $host "if ! grep '^nameserver' /etc/resolv.conf ;then echo 'nameserver 114.114.114.114' >> /etc/resolv.conf ; systemctl restart network; fi"
+    ssh $host "yum clean all"
+    ssh $host "yum install -y epel-release"
+    ssh $host "yum install -y python-pip"
+    ssh $host "pip install pymongo"
     ssh $host "unzip agent.zip; cd instance_monitor_agent.git; python setup.py install"
     ssh $host 'sed -i '\'"s/^server_host.*/server_host = \"${server_ip}\"/g"\'' /etc/VMAgent/VMAgent.conf'
-	ssh $host 'sed -i '\'"s/^mongodb_host.*/server_host = \"${mongo_ip}\"/g"\'' /etc/VMAgent/VMAgent.conf'
-	ssh $host "VMAgent > /dev/null"
+    ssh $host 'sed -i '\'"s/^mongodb_host.*/server_host = \"${mongo_ip}\"/g"\'' /etc/VMAgent/VMAgent.conf'
+    ssh $host "VMAgent-stop"
+    ssh $host "VMAgent > /dev/null"
 done
