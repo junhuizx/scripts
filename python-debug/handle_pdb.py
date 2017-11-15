513b4f20e855b21924ece0ed3c070834bb2540ee
zjh <junhui.zhang@newtouch.cn>
2017-11-15 14:50:48 +0800

    add handle_pdb.py

---
+++ python-debug/handle_pdb.py
@@ -0,0 +1,19 @@
+import os
+import signal
+import sys
+import time    
+
+def handle_pdb(sig, frame):
+    import pdb
+    pdb.Pdb().set_trace(frame)    
+
+def loop():
+    while True:
+        x = 'foo'
+        time.sleep(0.2)
+
+if __name__ == '__main__':
+    print signal.SIGUSR1
+    signal.signal(signal.SIGUSR1, handle_pdb)
+    print(os.getpid())
+    loop()
